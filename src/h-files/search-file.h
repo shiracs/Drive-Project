64f89f960a8019b641681df2163a9cbe12d1b0b4
Melameh3 <hemdat.melamed@gmail.com>
2025-11-19 21:51:38 +0200

    Dp 7- added file search tests (#4)
    * evrything written before tryng to compile
    
    * search test compile and working

---
+++ src/h-files/search-file.h
@@ -0,0 +1,4 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> search(const std::string& content_to_find);
---
+++ src/search-file.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "../h-files/search-file.h"
+
+std::vector<std::string> search(const std::string& content_to_find){
+    return {};
+}
