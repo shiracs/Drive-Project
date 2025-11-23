9691d00d59dd40e8fb1108caf880bba1a83c73b9
shiracs <shiracsw@gmail.com>
2025-11-23 12:51:17 +0200

    done (#8)

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     // 1. Check the environment variable for storage path. default is "./my_storage"
-    const char* envVar = std::getenv("DRIVE_PATH");
+    const char* envVar = std::getenv("STORAGE_PATH");
     std::string path = envVar ? envVar : "./my_storage";
 
     // Create a System object
