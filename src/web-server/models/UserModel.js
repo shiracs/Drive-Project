df061eaa495a0c5cc1c41f0d2d0f989b3e4e4e15
shiracs <shiracsw@gmail.com>
2025-12-17 23:44:08 +0200

    create new user endpoint done

---
+++ src/web-server/models/UserModel.js
@@ -0,0 +1,22 @@
+// In-memory user storage
+const users = [];
+
+/**
+ * User model to manage user data.
+ */
+const User = {
+  // Check if a user with the given username exists
+  exists: (username) => {
+    return users.some((u) => u.username === username);
+  },
+
+  // create and store a new user
+  create: (userData) => {
+    users.push(userData);
+    // TODO: get rid of this log before submission
+    console.log("Current Users in Store:", users);
+    return userData;
+  },
+};
+
+module.exports = User;
