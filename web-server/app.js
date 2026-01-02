57141cda03afd0413ec6b8e52fc7bb66eeee1e79
Shira Mendelson <shiracsw@gmail.com>
2026-01-02 14:21:32 +0200

    done

--- web-server/app.js
+++ web-server/app.js
@@ -4,11 +4,11 @@ import cors from 'cors';
 
 const app = express();
 const PORT =  5000;
-
 app.use(cors({
-    origin: '*', // לצרכי בדיקה בלבד, כדי לוודא שזו לא הבעיה
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 app.use(express.json());
 app.use('/api', apiRoutes);
