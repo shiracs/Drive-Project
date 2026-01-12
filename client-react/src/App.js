66f81510dc909731de89ce73499b667b8b8f9ca6
Melameh3 <hemdat.melamed@gmail.com>
2026-01-12 10:49:13 +0200

    jump to login in new run. and css for permision- long name

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { useEffect } from "react";
 
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -11,19 +12,42 @@ import FilePage from "./pages/FilePage";
 import DocumentViewPage from "./pages/DocumentViewPage";
 import MainLayout from "./components/layout/MainLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { SIDEBAR_PATHS } from "./consts/Sidebar";
 import SearchPage from "./pages/SearchPage";
-import { SIDEBAR_MENU, SIDEBAR_PATHS } from "./consts/Sidebar";
-import {
-  SHARED_API_URL,
-  OWNED_API_URL,
-  RECENT_API_URL,
-  STARRED_API_URL,
-  TRASH_API_URL,
-  SPAM_API_URL
-} from "./consts/Urls";
+import { API_BASE_URL } from "./consts/Urls";
+import { clearAuthData, getTokenHeader } from "./utils/auth";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
+  // Validate token on app load
+  useEffect(() => {
+    const validateToken = async () => {
+      const auth = getTokenHeader();
+      
+      // If there's a token, verify it's still valid
+      if (auth.Authorization) {
+        try {
+          const response = await fetch(`${API_BASE_URL}/users/me`, {
+            method: 'GET',
+            headers: auth
+          });
+          
+          // If token is invalid clear auth data and redirect to login
+          if (response.status === 401 || response.status === 403) {
+            clearAuthData();
+            window.location.href = '/login';
+          }
+        } catch (error) {
+          // If server is unreachable, clear token as it's likely invalid
+          clearAuthData();
+          window.location.href = '/login';
+        }
+      }
+    };
+    
+    validateToken();
+  }, []);
+
   return (
     <ThemeProvider>
       <Router>
@@ -39,61 +63,9 @@ function App() {
             </ProtectedRoute>
           }
         >
-          <Route path={SIDEBAR_PATHS.HOME} element={<FilePage />} />
           <Route
-            path="/shared"
-            element={
-              <FilePage
-                customUrl={SHARED_API_URL}
-                title={SIDEBAR_MENU.SHARED}
-              />
-            }
-          />
-          <Route
-            path="/my-storage"
-            element={
-              <FilePage
-                customUrl={OWNED_API_URL}
-                title={SIDEBAR_MENU.MY_STORAGE}
-              />
-            }
-          />
-          <Route
-            path="/recent"
-            element={
-              <FilePage
-                customUrl={RECENT_API_URL}
-                title={SIDEBAR_MENU.RECENT}
-                subTitle={"עד 10 אחרונים ששונו"}
-              />
-            }
-          />
-          <Route
-            path="/starred"
-            element={
-              <FilePage
-                customUrl={STARRED_API_URL}
-                title={SIDEBAR_MENU.STARRED}
-              />
-            }
-          />
-          <Route
-            path="/trash"
-            element={
-                <FilePage
-                  customUrl={TRASH_API_URL}
-                  title={SIDEBAR_MENU.TRASH}
-                />
-            }
-          />
-          <Route 
-              path="/spam" 
-              element={
-                  <FilePage 
-                      customUrl={SPAM_API_URL} 
-                      title={SIDEBAR_MENU.SPAM} 
-                  />
-              } 
+            path={`/${SIDEBAR_PATHS.HOME}/:folderId?`}
+            element={<FilePage />}
           />
           <Route path="/search" element={<SearchPage />} />
           <Route
